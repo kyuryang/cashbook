6cb9f78223afbe75f4d1ede65796fe2549165112
GDJ46 <GDJ46@DESKTOP-NT249FG>
2022-04-27 17:34:49 +0900

    member 탈퇴 일부 구현

--- cashbook/src/main/java/dao/MemberDao.java
+++ cashbook/src/main/java/dao/MemberDao.java
@@ -44,7 +44,39 @@ public class MemberDao {
 		
 	}
 	//회원수정
+	public String updateMember(Member member) {					//회원 수정
+		
+		String sql="update member set ";
+//		UPDATE stats SET cnt = cnt+1 WHERE DAY = CURDATE()
+		
+		return "";
+	}
 	//회원탈퇴
+	public void deleteMemer(Member member) {
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		String sql="delete from member where member_id=? and member_pw=?";
+		int row=0;
+		
+	      try {
+		         conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cashbook","root","java1234");
+		         stmt = conn.prepareStatement(sql);
+		         stmt.setString(1, member.getMemberId());
+		         stmt.setString(2, member.getMemberPw());
+		         row =stmt.executeUpdate();
+		        
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		      } finally {
+		         try {
+		            conn.close();
+		         } catch (SQLException e) {
+		            e.printStackTrace();
+		         }
+		      }
+		      System.out.println(row + "<--탈퇴된 회원 수 ");
+		      
+	}
 	//회원정보
 	
 	
